7466f762261c814369974d3cc03f3cff37964180
Jonathan Berger <john@3kd.be>
2016-05-19 00:31:19 +0200

    styling, README and useful animations

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # i-love-chips-16
 
-`npm start`. YOLO.
+## Running on OSX or Linux
+
+* `npm install`
+* `bower install`
+* `npm dry` (Dry Run, without I2C)
+
+## Running on Raspi
+
+Libraries for I2C have to be installed and built separately on
+Raspi since build fails on OSX systems.
+
+* `npm install`
+* `sudo npm -g install bower`
+* `npm install i2c-bus pca9685`
+* `bower install`
+* `npm dev` (run with I2C)
--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -4,4 +4,42 @@
   $('body').communication({
     socket: socket
   });
+
+  var title = new MovingTitle("NERVOUS BREAKDOWN ⚠️⚠️⚠️⚠️⚠️⚠ ", 300, 33);
+  title.init();
 }(jQuery));
+
+
+function MovingTitle(writeText, interval, visibleLetters) {
+  var _instance = {};
+
+  var _currId = 0;
+  var _numberOfLetters = writeText.length;
+
+  function updateTitle() {
+    _currId += 1;
+    if(_currId > _numberOfLetters - 1) {
+      _currId = 0;
+    }
+
+    var startId = _currId;
+    var endId = startId + visibleLetters;
+    var finalText;
+    if(endId < _numberOfLetters - 1) {
+      finalText = writeText.substring(startId, endId);
+    } else {
+      var cappedEndId = _numberOfLetters;
+      endId = endId - cappedEndId;
+
+      finalText = writeText.substring(startId, cappedEndId) +     writeText.substring(0, endId);
+    }
+
+    document.title = finalText;
+  }
+
+  _instance.init = function() {
+    setInterval(updateTitle, interval);
+  };
+
+  return _instance;
+}
